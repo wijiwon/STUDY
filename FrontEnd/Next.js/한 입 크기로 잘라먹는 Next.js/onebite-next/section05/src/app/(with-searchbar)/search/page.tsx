b2833eb5ad02c5782c4c2478de3ac862e7cbcd0c
wijiwon <dnlwldnjs5@gmail.com>
2024-09-04 15:17:36 +0900

    한입크기로 잘라먹는 Next 5-3 수강완료

---
+++ FrontEnd/Next.js/한 입 크기로 잘라먹는 Next.js/onebite-next/section05/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,26 @@
+import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    { cache: "force-cache" }
+  );
+  if (!res.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const searchBooks: BookData[] = await res.json();
+  return (
+    <div>
+      {searchBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
